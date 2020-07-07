74b2bbaa3ba00992036a290e1e531c4e80d2e18c
Pelon <pelon@Mac-mini.local>
2020-07-07 14:07:57 -0600

    myfunctions

---
+++ Basic_JavaScript_2.js
@@ -0,0 +1,12 @@
+function method_1() {
+    var x = 3 
+    var z = 4
+    document.getElementById("number").innerHTML= x+z 
+}
+
+
+function myFunction () {
+    var sentence = "i am learning";
+    sentence += "a lot from this book!";
+    document.getElementById("concatenate").innerHTML=sentence;
+}
