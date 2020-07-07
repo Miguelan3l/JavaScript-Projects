9719653953cf3f9be860e725439d333ae064420d
Pelon <pelon@Mac-mini.local>
2020-07-07 12:07:43 -0600

    simple java script

---
+++ JavaScript_file.js
@@ -0,0 +1,10 @@
+alert("Solve math problem");// an alert box that welcomes client
+
+function My_First_Function () {//Definin a function and naming it   
+    var str ="12+12=24";//Defining a varible and giving it a string value
+    var result = str.fontcolor("green");//using the fontcolor method on //str variable
+    document.getElementById("Green_Text").innerHTML = result;// of reslt into HTML element with "Green_Text" id
+}
+ document.write(`Can you solve the math probelm below ? To solve click the botton!`) //aksing user to solve problem 
+
+ 
